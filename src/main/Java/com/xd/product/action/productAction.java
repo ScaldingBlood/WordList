ead291e1e3f6121a4227334c2954a441c58c516e
xichangzun <xichangzun@gmail.com>
2016-06-23 15:30:19 +0800

    change search function

--- src/main/Java/com/xd/product/action/productAction.java
+++ src/main/Java/com/xd/product/action/productAction.java
@@ -61,28 +61,61 @@ public class productAction extends BaseAction{
     {
         HttpServletRequest request = ServletActionContext.getRequest();
         int parameter = Integer.parseInt(request.getParameter("parameter"));
-        //String spelling = request.getParameter("spelling");
+        String spelling = request.getParameter("spelling");
         HttpSession session = request.getSession();
         UUID id = UUID.fromString((String) session.getAttribute("id"));
         JSONArray result = new JSONArray();
-
-        try {
-            List xcz = this.getService().FindByParameter(id,parameter);
-            if (xcz != null && xcz.size() > 0) {
-                for (int i = 0; i < xcz.size(); i++) {
-                    Object[] obj = (Object[]) xcz.get(i);
-                    JSONObject p = new JSONObject();
-                    p.put("id",id);
-                    p.put("spelling",(String)obj[0]);
-                    p.put("definition",(String)obj[1]);
-                    p.put("sentences",(String)obj[2]);
-                    p.put("date",new Date(((Timestamp)obj[3]).getTime()));//may provoke exception
-                    result.add(p);
-                }
+        if(spelling != null)
+        {
+            HttpServletResponse response = ServletActionContext.getResponse();
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json; charset=utf-8");
+            String sql = "SELECT SPELLING,DEFINITION,SENTENCES,DATE FROM WORDLIST.WORD WHERE ID = '"+id.toString()+"' SPELLING = '"+spelling+"'";
+            List xcz = this.getService().find(sql);
+            if(xcz ==null)
+            {
+                return;
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
+            else
+            {
+                JSONObject p = new JSONObject();
+                Object[] obj = (Object[]) xcz.get(0);
+                p.put("id", id);
+                p.put("spelling", (String) obj[0]);
+                p.put("definition", (String) obj[1]);
+                p.put("sentences", (String) obj[2]);
+                p.put("date", new Date(((Timestamp) obj[3]).getTime()).toString());
+
+                try {
+                    response.getWriter().append(p.toJSONString());
+                    response.getWriter().close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return;
+            }
         }
+        else
+        {
+            try {
+                List xcz = this.getService().FindByParameter(id,parameter);
+                if (xcz != null && xcz.size() > 0) {
+                    for (int i = 0; i < xcz.size(); i++) {
+                        Object[] obj = (Object[]) xcz.get(i);
+                        JSONObject p = new JSONObject();
+                        p.put("id",id);
+                        p.put("spelling",(String)obj[0]);
+                        p.put("definition",(String)obj[1]);
+                        p.put("sentences",(String)obj[2]);
+                        p.put("date",new Date(((Timestamp)obj[3]).getTime()).toString());//may provoke exception
+                        result.add(p);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
         HttpServletResponse response = ServletActionContext.getResponse();
         String JSONstr = result.toJSONString();
         response.setCharacterEncoding("UTF-8");
@@ -110,4 +143,5 @@ public class productAction extends BaseAction{
         setWord(data,id, spelling, definition, sentences, date);
         this.getService().update(data);
     }
+
 }
